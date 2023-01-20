822df496634b3b2d28efafcb57cf12a944143f37
Prince Narteh <prinarteh@gmail.com>
2023-01-20 17:49:55 +0000

    finish styling card component

--- app/Card.tsx
+++ app/Card.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { BiEdit } from "react-icons/bi";
+import { MdOutlineDelete } from "react-icons/md";
 
 const Card = () => {
   return (
-    <div className="relative bg-teal-500 max-w-md p-5 h-52 rounded-lg">
-      <h3 className="truncate font-bold text-xl mb-3">
+    <div className="relative bg-rose-500 max-w-md p-5 h-52 rounded-lg group overflow-hidden cursor-pointer">
+      <h3 className="truncate font-bold text-xl mb-2 text-gray-800">
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea consequatur
         esse exercitationem aperiam minus laboriosam ipsum illo. Sunt sint amet,
         eos dolor numquam, repudiandae accusamus ad pariatur perspiciatis illum
@@ -18,7 +20,13 @@ const Card = () => {
         repellendus fugit, suscipit ab odio labore aliquam eum similique
         laudantium inventore repudiandae non.
       </p>
-      <p className="absolute bottom-5">20th Jan, 2023</p>
+      <p className="absolute bottom-5 text-gray-800/80">20th Jan, 2023</p>
+
+      {/* Options */}
+      <div className="absolute h-full right-0 top-0 bg-white bg-opacity-10 border-l border-l-white text-white flex flex-col justify-center px-2 text-2xl gap-5 transition-all translate-x-14 group-hover:-translate-x-0 duration-300 shadow-xl">
+        <BiEdit className="cursor-pointer hover:scale-110" />
+        <MdOutlineDelete className="cursor-pointer hover:scale-110" />
+      </div>
     </div>
   );
 };
