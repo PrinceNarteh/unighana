6f17ff7b3af10ea62d9416b8aad3a39840ca65d6
Prince Narteh <prinarteh@gmail.com>
2023-01-21 08:07:01 +0000

    configure axios

--- pages/api/notes/index.ts
+++ pages/api/notes/index.ts
@@ -1,11 +1,15 @@
 import { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
+import { createNoteDto } from "@/utils/validations";
 import dbConnect from "@/config/dbConnect";
 import Note from "@/models/note";
-import { createNoteDto } from "@/utils/validations";
 
 const getAllNotes = async (req: NextApiRequest, res: NextApiResponse) => {
-  const notes = await Note.find({});
-  return res.status(200).json({ notes });
+  try {
+    const notes = await Note.find({});
+    return res.status(200).json({ notes });
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
 };
 
 const createNote = async (req: NextApiRequest, res: NextApiResponse) => {
@@ -19,8 +23,8 @@ const createNote = async (req: NextApiRequest, res: NextApiResponse) => {
     const note = await Note.create(response.data);
 
     res.status(201).json({ note });
-  } catch (error: any) {
-    res.status(500).json({ error: error.message });
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
   }
 };
 
